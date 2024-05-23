64f8a1ec2373a9643f513ea8632ec64eb6dfdc55
giuseppe d'erminio <giuseppede90@gmail.com>
2024-05-23 06:44:05 +0200

    regole

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,48 +4,107 @@
         <div class="col-12 m-0">
             
             <div class="footerhome">
-                <div class="container ">
+                <div class="container-fluid ">
                     
                     <div class="row justify-content-between">
-                        <div class="col-9 mt-2 h1cam ">       
-                            
-                            
-                            <div class="d-flex justify-content-start">      
+                        <div class="col-12 col-md-6 mt-2 ">       
+                            <div class="h1cam d-flex p-3">
                                 
-                                <i class="fa-solid fa-phone m-1" style="color: #FFD43B;"></i>
-                                <span style="color: black;"> +39 0000000000</span>
+                                <div class="d-flex justify-content-start p-2">      
+                                    <div class="d-flex">
+                                    <i class="fa-solid fa-phone m-1" style="color: #FFD43B;"></i>
+                                </div>
+                                    <div style="color: black;"> +39 3314931556</div>
+                                </div>
+                                <div class="d-flex justify-content-start">
+                                    <div class="d-flex">
+                                    <i class="fa-solid fa-house m-1" style="color: #FFD43B;"></i>
+                                </div>
+                                    <span style="color: black;">Via dei Bastioni 13, 86039 Termoli, Italia</span>
+                                </div>
+                                <div class="d-flex justify-content-start">
+                                    <div class="d-flex">
+                                    <i class="fa-regular fa-envelope m-1" style="color: #FFD43B;"></i>
+                                </div>
+                                    <div style="color: black;">Giuliam170297@gmail.com</div>
+                                </div>
+                                
                             </div>
-                            <div class="d-flex justify-content-start">
-                                <i class="fa-solid fa-house m-1" style="color: #FFD43B;"></i>
-                                <span style="color: black;">Via dei Bastioni 13, 86039 Termoli, Italia</span>
-                            </div>
-                            <div class="d-flex justify-content-start">
-                                <i class="fa-regular fa-envelope m-1" style="color: #FFD43B;"></i>
-                                <span style="color: black;">giuppa@chemailstupida.com</span>
-                            </div>
+                            
+                            
+{{--                             
+                            icone facebook linkedin,ecc..
+                            
+                            <div class="d-flex justify-content-center align-items-center p-2">
+                                
+                                <span>
+                                    <a href="">
+                                        <i class="fa-brands fa-facebook fa-lg p-2"></i>
+                                    </a>
+                                </span>
+                                <span>
+                                    <a href="">
+                                        <i class="fa-brands fa-instagram fa-lg p-2"></i>
+                                    </a>
+                                </span>
+                                <span>
+                                    <a href="">
+                                        <i class="fa-brands fa-linkedin fa-lg p-2"></i>
+                                    </a>
+                                </span>
+                                
+                                
+                                
+                            </div> --}}
                         </div>
                         
-                        
-                        
-                        
-                        <div class="col-3 d-flex justify-content-center align-items-center">
+                        <div class="col-12 col-md-6 d-flex justify-content-around align-items-center p-3">
                             
-                            <span>
-                                <a href="">
-                                    <i class="fa-brands fa-facebook fa-lg p-2"></i>
-                                </a>
-                            </span>
-                            <span>
-                                <a href="">
-                                    <i class="fa-brands fa-instagram fa-lg p-2"></i>
-                                </a>
-                            </span>
-                            <span>
-                                <a href="">
-                                    <i class="fa-brands fa-linkedin fa-lg p-2"></i>
-                                </a>
-                            </span>
+                            <!-- Botone per i servizi -->
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#serviziModal">Servizi</button>
                             
+                            <!-- Botone per il regolamento -->
+                            <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#regolamentoModal">Regolamento</button>
+                            
+                            <!-- Modal per i servizi -->
+                            <div class="modal fade" id="serviziModal" tabindex="-1" role="dialog" aria-labelledby="serviziModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="serviziModalLabel">Servizi</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <ul>
+                                                <li>Colazione inclusa</li>
+                                                <li>Wi-Fi gratuito</li>
+                                                <li>Parcheggio privato</li>
+                                                <li>...</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <!-- Modal per il regolamento -->
+                            <div class="modal fade" id="regolamentoModal" tabindex="-1" role="dialog" aria-labelledby="regolamentoModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="regolamentoModalLabel">Regolamento</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>Il regolamento del nostro B&B prevede...</p>
+                                            <p>...</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                             
                             
                         </div>
